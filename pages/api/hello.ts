b234ba9d8f2b3463af6b49eea0bce143e477d557
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-08 22:48:47 +0700

    show API response

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  [name: string]: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+  const { data } = await axios.get('http://culinar-ml.herokuapp.com')
+
+  console.log(data)
+
+  res.status(200).json(data)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ "message": "Server error" })
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
+import axios from 'axios'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 import styles from '../styles/Home.module.css'
 
+const defaultValue = {
+  data: "",
+  message: "",
+  status_code: 0,
+}
+
 const Home: NextPage = () => {
+  const [data, setData] = React.useState(defaultValue)
+
+  React.useEffect(() => {
+    const fetchHello = async () => {
+      const { data } = await axios.get('http://culinar-ml.herokuapp.com')
+
+      setData(data)
+    }
+
+    fetchHello()
+  })
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +35,14 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://culinar.in" target="_blank" rel="noopener noreferrer">Culinar</a>
         </h1>
+        <div className={styles.card}>
+          <h2>API response</h2>
+          <p className={styles.code}>
+            {`data: ${data.data}
+            message: ${data.message}
+            status_code: ${data.status_code}`}
+          </p>
+        </div>
       </main>
 
       <footer className={styles.footer}>
