688d56aa9a4750aded6573b0e044f456266c8b50
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-10 00:50:14 +0700

    update index and app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
-    <div className='container mx-auto p-4'>
-      <Component {...pageProps} />
-    </div>
+    <Navbar />
+    <Component {...pageProps} />
   </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Container from '../components/Container'
+import styles from '../styles/pages/Home.module.css'
 
 interface HomeProps {
   data: string;
@@ -14,21 +14,32 @@ export async function getStaticProps() {
   const { data } = await axios.get('https://culinar-ml.herokuapp.com')
 
   return {
-    props: {...data}
+    props: { ...data },
   }
 }
+
 const Home: NextPage<HomeProps> = ({ data, message, status_code }) => {
   return (
-    <div className={styles.container}>
+    <Container className='py-4'>
       <Head>
         <title>Culinar</title>
-        <meta name="description" content="culinar.in is restaurant recommendation app powered by machine learning." />
+        <meta
+          name="description"
+          content="culinar.in is restaurant recommendation app powered by machine learning."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://culinar.in" target="_blank" rel="noopener noreferrer">Culinar</a>
+          Welcome to{' '}
+          <a
+            href="https://culinar.in"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Culinar
+          </a>
         </h1>
         <div className={styles.card}>
           <h2>API response</h2>
@@ -41,15 +52,11 @@ const Home: NextPage<HomeProps> = ({ data, message, status_code }) => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://culinar.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://culinar.in" target="_blank" rel="noopener noreferrer">
           Copyright 2022 Culinar<sup>&copy;</sup>
         </a>
       </footer>
-    </div>
+    </Container>
   )
 }
 
