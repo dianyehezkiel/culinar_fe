7fde4fd1f1b342feac0830b88f1cd0f2b90ba765
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-09 09:50:20 +0700

    add container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <div className='container mx-auto p-4'>
+      <Component {...pageProps} />
+    </div>
+  </>
+  )
 }
 
 export default MyApp
