02d77503a20aec8c95195715e8493eaea2b08ce6
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-10 12:32:28 +0700

    add footer to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
     <Navbar />
     <Component {...pageProps} />
+    <Footer />
   </>
   )
 }
