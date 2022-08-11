d1d43da466e6af8114245797c3e179f0df5183f5
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-11 16:00:44 +0700

    update container

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,5 @@
+import { classNames } from "../utils"
+
 interface ContainerProps {
   children: React.ReactNode;
   className?: string;
@@ -6,7 +8,10 @@ interface ContainerProps {
 const Container: React.FC<ContainerProps> = ({ children, className }) => {
   return (
     <div
-      className={`lg:container lg:mx-auto px-4 ${className}`}
+      className={classNames(
+        'md:container lg:w-[1024px] md:mx-auto px-8',
+        className ? className : "",
+      )}
     >
       {children}
     </div>
