9a43d78451cdbb86225d0232be43fe4f721fe824
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-13 11:24:30 +0700

    add state to store img source and make src param optional

--- components/ImageWithFallback.tsx
+++ components/ImageWithFallback.tsx
@@ -3,24 +3,36 @@ import Image from 'next/image'
 import React from 'react'
 
 interface IWFProps {
-  src: string;
+  src?: string;
   alt: string;
   fallBackSrc?: string;
 }
 
 const ImageWithFallback: React.FC<IWFProps> = ({
-  src,
+  src = fallback.src,
   alt,
   fallBackSrc = fallback.src,
 }) => {
   const [imageError, setImageError] = React.useState(false)
+  const [imgSource, setImgSource] = React.useState('')
+
+  if (imgSource !== src) {
+    setImageError(false)
+    setImgSource(src)
+  }
+
+  const handleImageError = () => {
+    setImageError(true)
+  }
+
   return (
     <Image
       className='object-cover'
       src={imageError ? fallBackSrc : src}
+      loading='lazy'
       alt={alt}
       layout='fill'
-      onError={() => setImageError(true)}
+      onError={handleImageError}
     />
   )
 }
